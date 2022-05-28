111b101f03c11428a740c1a766d9649a8127fb6f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-05-28 16:04:23 +0530

    added comment

--- src/components/ingredients/IngredientForm.js
+++ src/components/ingredients/IngredientForm.js
@@ -1,7 +1,7 @@
 import React  from "react";
 import './IngredientForm.css';
 import Card from '../UI/Card.css';
- const IngredientForm = React.memo(props =>{//
+ const IngredientForm = React.memo(props =>{ // to prevent unwanted rerendering
 
 
     return(
