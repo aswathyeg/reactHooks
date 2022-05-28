58c348c07933d0bb55c54bc1a31e391aee8e1019
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-05-28 16:03:01 +0530

    wraped with memo

---
+++ src/components/ingredients/IngredientForm.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import './IngredientForm.css';
+import Card from '../UI/Card.css';
+ const IngredientForm = React.memo(props =>{//
+
+
+    return(
+        <div>
+
+        </div>
+    )
+ });
+
+export default IngredientForm;
