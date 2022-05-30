9ebb8cc7cdba11249fd892c974a110f616518cbf
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-05-30 06:23:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import IngredientForm from './components/Ingredients/IngredientForm';
 import Ingredients from './components/Ingredients/Ingredients';
-import Card from './components/UI/Card';
+
 function App() {
+
   return (
+
     <div>
-      
-{/* <Ingredients /> */}
-<Card>
-<input type="text"/>
-</Card>
+      <Ingredients />
     </div>
   )
 };
