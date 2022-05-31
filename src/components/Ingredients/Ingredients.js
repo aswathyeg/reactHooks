d63764313ec9b56e61f0856fb1a9d3670dbfdb12
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-05-31 20:12:30 +0530

    added hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,13 +1,30 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './IngredientForm.css';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList';
 
-const Ingredients = () => {
+const Ingredients = (props) => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredients = ingredient =>{ //title and amount
+    
+    setIngredients(prevIngredient =>
+      [...prevIngredient,
+        {id:Math.random().toString,...ingredient}]
+    )
+
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm  addIngredients={addIngredients}/> {/*will get input from this component */}
 
+      <section className="ingredient-form">
+        <Search />
+        <IngredientList ingredients = {ingredients} />
+        {/* output */}
+      </section>
     </div>
   );
 }
