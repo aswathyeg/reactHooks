a3061edfb942b26a79780345b4f06e9594a0c2a8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-01 22:51:12 +0530

    added remove function

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -4,15 +4,21 @@ import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
 
-const Ingredients = (props) => {
+const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
   const addIngredients = ingredient =>{ //title and amount
     
     setIngredients(prevIngredient =>
       [...prevIngredient,
-        {id:Math.random().toString,...ingredient}]
-    )
+        {id:Math.random().toString(),...ingredient}]
+    );
+
+  };
+  const removeHandler = ingredientId => {
+    setIngredients(prevIngredient => 
+      prevIngredient.filter(ingredient => ingredient.id !==ingredientId));//filter-if the function returns true, that item will remains, if false, that item get deleted
+    
 
   }
 
@@ -22,7 +28,7 @@ const Ingredients = (props) => {
 
       <section className="ingredient-form">
         <Search />
-        <IngredientList ingredients = {ingredients} />
+        <IngredientList ingredients = {ingredients} onRemove= {removeHandler} />
         {/* output */}
       </section>
     </div>
