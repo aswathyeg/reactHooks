d1da8a2c85a7742307082341651f7ba97f78f18c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-01 22:53:13 +0530

    updated

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [filterInput,setFilterInput] =useState('');
+  const onType= (e) =>{
+    setFilterInput(e.target.value);
+
+  }
   return (
     <section className="search">
       <Card>    
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" onChange={onType} />
         </div>
       </Card>
     </section>
