37af65a69ef1924b95ba4e138869c898f635075d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-07-08 10:51:12 +0530

    added useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,46 +1,66 @@
-import React, { useState } from 'react';
-import './IngredientForm.css';
-import IngredientForm from './IngredientForm';
-import Search from './Search';
-import IngredientList from './IngredientList';
+import React, { useEffect, useState } from "react";
+import "./IngredientForm.css";
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredients = ingredient =>{ //title and amount
-    fetch('https://react-hooks-353b0-default-rtdb.firebaseio.com/ingredients.json',{
-      method:'POST',
-      body:JSON.stringify(ingredient),
-      headers:{'Content-Type':'application/json'}
-    }).then(response => { //promise-will not execute immediatly, only when above code done
-      setIngredients(prevIngredient =>
-        [...prevIngredient,
-          {id:Math.random().toString(),...ingredient}]
-      ); 
-        });
-    
-
+  useEffect(() => {
+    fetch(
+      "https://react-ingredients-e97be-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const identifier in responseData) {
+          loadedIngredients.push({
+            id: identifier,
+            amount: responseData[identifier].amount,
+            title: responseData[identifier].title,
+          });
+        }
+        setIngredients(loadedIngredients);
+        console.log(loadedIngredients);
+      });
+  }, []);
+  const addIngredients = (ingredient) => {
+    //title and amount
+    fetch(
+      "https://react-ingredients-e97be-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((response) => {
+      //promise-will not execute immediatly, only when above code done
+      setIngredients((prevIngredient) => [
+        ...prevIngredient,
+        { id: Math.random().toString(), ...ingredient },
+      ]);
+    });
   };
-  const removeHandler = ingredientId => {
-    setIngredients(prevIngredient => 
-      prevIngredient.filter(ingredient => ingredient.id !==ingredientId));//filter-if the function returns true, that item will remains, 
-      //if false, that item get deleted
-      // ingredient(placeholder) -is an object ,
-    
-
-  }
+  const removeHandler = (ingredientId) => {
+    setIngredients((prevIngredient) =>
+      prevIngredient.filter((ingredient) => ingredient.id !== ingredientId)
+    ); //filter-if the function returns true, that item will remains,
+    //if false, that item get deleted
+    // ingredient(placeholder) -is an object ,
+  };
 
   return (
     <div className="App">
-      <IngredientForm  addIngredients={addIngredients}/> {/*will get input from this component */}
-
+      <IngredientForm addIngredients={addIngredients} />{" "}
+      {/*will get input from this component .passing callback function here*/}
       <section className="ingredient-form">
         <Search />
-        <IngredientList ingredients = {ingredients} onRemove= {removeHandler} />
+        <IngredientList ingredients={ingredients} onRemove={removeHandler} />
         {/* output */}
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
