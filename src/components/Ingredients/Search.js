c774080eef42533b3d6eb9d31795d7f2592b7547
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-07-08 10:51:43 +0530

    added hooks

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
-  const [filterInput,setFilterInput] =useState('');
-  const onType= (e) =>{
+const Search = React.memo((props) => {
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://react-ingredients-e97be-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+      });
+  }, []);
+  const [filterInput, setFilterInput] = useState("");
+  const onType = (e) => {
     setFilterInput(e.target.value);
-
-  }
+  };
   return (
     <section className="search">
-      <Card>    
+      <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" onChange={onType} />
+          <input type="text" onChange={onType} value={enteredFilter} />
         </div>
       </Card>
     </section>
