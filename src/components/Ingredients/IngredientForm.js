1cac7ea3250ef207390f53bdc625e6dbaaf0e8ee
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-07-08 14:52:26 +0530

    added loading indicator

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,60 +1,46 @@
-import React, { useState }  from "react";
-import './IngredientForm.css';
-import Card from '../UI/Card.css';
+import React, { useState } from "react";
+import "./IngredientForm.css";
+import Card from "../UI/Card.css";
+import LoadingIndicator from "../UI/LoadingIndicator";
 
-    const IngredientForm = React.memo(props =>{  // to prevent unwanted rerendering
-      
-      const [title,setTitle] = useState('');
-      const [amount,setInputAmount] = useState('');
-      
+const IngredientForm = React.memo((props) => {
+  // to prevent unwanted rerendering
 
-  
-        const submitForm =(e)=>{
-          e.preventDefault();
-          props.addIngredients({
-            title:title,
-            amount:amount
-          });
+  const [title, setTitle] = useState("");
+  const [amount, setInputAmount] = useState("");
 
-        }
-        const getName =(e) =>{
-          setTitle(e.target.value);
-        
-               
-        }
-        const getAmount =(e) =>{
-          setInputAmount(e.target.value);
-        }
+  const submitForm = (e) => {
+    e.preventDefault();
+    props.addIngredients({
+      title: title,
+      amount: amount,
+    });
+  };
+  const getName = (e) => {
+    setTitle(e.target.value);
+  };
+  const getAmount = (e) => {
+    setInputAmount(e.target.value);
+  };
 
-    return(
-     
-
-      <section className="ingredient-form">
-
-        <form onSubmit={submitForm} >
-
-          <div className="form-control">
-            <label>Name</label>
-            <input type="text"
-            id="title"
-            value={title}
-            onChange={getName} />
-          </div>
-          <div className="form-control">
-            <label>Amount</label>
-            <input type="number"
-            id="amount"
-           
-            onChange={getAmount}/>
-          </div>
-          <div>
-            <button>Add Ingredients</button>
-          </div>
-        </form>
-   
-        </section>
-             
-        )
- });
+  return (
+    <section className="ingredient-form">
+      <form onSubmit={submitForm}>
+        <div className="form-control">
+          <label>Name</label>
+          <input type="text" id="title" value={title} onChange={getName} />
+        </div>
+        <div className="form-control">
+          <label>Amount</label>
+          <input type="number" id="amount" onChange={getAmount} />
+        </div>
+        <div>
+          <button>Add Ingredients</button>
+          {props.loading && <LoadingIndicator />}
+        </div>
+      </form>
+    </section>
+  );
+});
 
 export default IngredientForm;
