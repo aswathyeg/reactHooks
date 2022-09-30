8742f8e717743b0ff4796ac1278e5d7bc8a4b8a1
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-09-30 16:24:08 +0530

    added styles

--- src/components/shoppingCart/Header.js
+++ src/components/shoppingCart/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Styles.css";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <span className="header">context API</span>
+      <ul className="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Header;
