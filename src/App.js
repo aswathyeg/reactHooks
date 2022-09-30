dd2a12ec32eaeec6f5b9010e57cc023929dc3819
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-09-30 12:55:47 +0530

    added Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
-import Ingredients from "./components/Ingredients/Ingredients";
-import { AuthContext } from "./context/auth-context";
-import Auth from "./components/Auth";
+// import Ingredients from "./components/Ingredients/Ingredients";
+// import { AuthContext } from "./context/auth-context";
+// import Auth from "./components/Auth";
+import Header from "./components/shoppingCart/Header";
 
 function App() {
-  const authContext = useContext(AuthContext);
-  let content = <Auth />;
-  if (authContext.isAuth) {
-    content = <Ingredients />;
-  }
-  return content;
+  // const authContext = useContext(AuthContext);
+  // let content = <Auth />;
+  // if (authContext.isAuth) {
+  //   content = <Ingredients />;
+  // }
+  // return content;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
