e1a1352b47a542a17bd0fdd3bdf394f8e4abb5ac
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-03 16:33:40 +0530

    updated

--- src/components/shoppingCart/Home.js
+++ src/components/shoppingCart/Home.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import faker from "faker";
 const Home = () => {
-  return <div>Home</div>;
+  const productArray = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),
+  }));
+  const [products] = useState(productArray);
+  console.log(productArray);
+  return (
+    <div className="productContainer">
+      {products.map((prod) => (
+        <span>{prod.name}</span>
+      ))}
+    </div>
+  );
 };
 export default Home;
