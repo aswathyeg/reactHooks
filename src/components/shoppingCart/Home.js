44d2cda241d6d9c097e736eeddc2e30a032b431c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-04 17:52:23 +0530

    added seed

--- src/components/shoppingCart/Home.js
+++ src/components/shoppingCart/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
 import SingleProduct from "./SingleProduct";
+import "./Styles.css";
+faker.seed(100);
 const Home = () => {
   const productArray = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
