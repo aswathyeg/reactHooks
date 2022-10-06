697c767904fea463dfc2d4a7d1aad662920bd873
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-06 22:28:53 +0530

    added context

--- src/components/shoppingCart/Cart.js
+++ src/components/shoppingCart/Cart.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import SingleProduct from "./SingleProduct";
+import { Cart } from "./Context";
 const CartPage = () => {
+  const { cart } = useContext(Cart);
+  const [total, setTotal] = useState();
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + Number(curr.price), 0));
+  }, [cart]);
   return (
     <div>
       <span style={{ fontSize: 30 }}>My cart</span>
-      <span style={{ fontSize: 30 }}>Total:{100}</span>
+      <br />
+      <span style={{ fontSize: 30 }}>Total:{total}</span>
+      <div className="productContainer">
+        {cart.map((prod) => (
+          <SingleProduct prod={prod} key={prod.id} />
+        ))}
+      </div>
     </div>
   );
 };
