10e01c2c0ed620e24bbaa7c1b70a0e601b32dc29
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-06 19:11:29 +0530

    added style

--- src/components/shoppingCart/Cart.js
+++ src/components/shoppingCart/Cart.js
@@ -1,5 +1,10 @@
 import React from "react";
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <span style={{ fontSize: 30 }}>My cart</span>
+      <span style={{ fontSize: 30 }}>Total:{100}</span>
+    </div>
+  );
 };
 export default Cart;
