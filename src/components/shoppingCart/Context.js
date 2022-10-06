2f1378906d9d31444538475f9cd6cdc29ac9220f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-06 21:54:12 +0530

    provided the values

--- src/components/shoppingCart/Context.js
+++ src/components/shoppingCart/Context.js
@@ -1,5 +1,8 @@
-import React from "react";
-const Context = () => {
-  return <div></div>;
+import React, { createContext, useState } from "react";
+const Cart = createContext();
+const [cart, setCart] = useState([]);
+const Context = ({ children }) => {
+  // below code will make these value available to all required components
+  return <Cart.Provider value={{ cart, setCart }}>{children}</Cart.Provider>;
 };
 export default Context;
