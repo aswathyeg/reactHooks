9dc5d36e2883d2d83c578e99d33d9ea9e1e8672e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-06 22:29:20 +0530

    exported context

--- src/components/shoppingCart/Context.js
+++ src/components/shoppingCart/Context.js
@@ -1,7 +1,8 @@
 import React, { createContext, useState } from "react";
-const Cart = createContext();
-const [cart, setCart] = useState([]);
+export const Cart = createContext();
+
 const Context = ({ children }) => {
+  const [cart, setCart] = useState([]);
   // below code will make these value available to all required components
   return <Cart.Provider value={{ cart, setCart }}>{children}</Cart.Provider>;
 };
