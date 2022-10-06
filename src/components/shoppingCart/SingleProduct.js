65094ddd45f117817f56ebf96e6337cf05e09e5d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-06 19:31:14 +0530

    added ternary operator

--- src/components/shoppingCart/SingleProduct.js
+++ src/components/shoppingCart/SingleProduct.js
@@ -8,7 +8,25 @@ const SingleProduct = ({ prod, cart, setCart }) => {
         <span style={{ fontWeight: 700 }}>{prod.name}</span>
         <span>${prod.price.substring(0, 3)}</span>
       </div>
-      <button className="add">Add to cart</button>
+      {cart.includes(prod) ? (
+        <button
+          className="remove"
+          onClick={() => {
+            setCart(cart.filter((c) => c.id !== prod.id));
+          }}
+        >
+          Remove from cart
+        </button>
+      ) : (
+        <button
+          className="add"
+          onClick={() => {
+            setCart([...cart, prod]);
+          }}
+        >
+          Add to cart
+        </button>
+      )}
     </div>
   );
 };
