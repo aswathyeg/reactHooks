d96d13a3884ea25b786847b69074298628d1060e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-10 23:55:08 +0530

    imported bootstrap

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 // import AuthContextProvider from "./context/auth-context";
 import Context from "./components/shoppingCart/Context";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Context>
