c4ab617ef6a190d71fe6bf20113faa83babfdcf8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-11 23:44:10 +0530

    path updated

--- src/components/shopping-context-reducer/Context.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useState } from "react";
-export const Cart = createContext();
-const Context = ({ childrern }) => {
-  const [cart, setCart] = useState([]);
-  return <Cart.Provider value={{ cart, setCart }}>{childrern}</Cart.Provider>;
-};
-export default Context;
