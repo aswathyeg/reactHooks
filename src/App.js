5ec0a622eaccd58248cc97254a3eb78a61ea632e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-11 13:52:29 +0530

    updated components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CartPage from "./components/shoppingCart/CartPage";
+//import CartPage from "./components/shoppingCart/CartPage";
 //import Cart from "./components/shoppingCart/CartPage";
 // import Ingredients from "./components/Ingredients/Ingredients";
 // import { AuthContext } from "./context/auth-context";
 // import Auth from "./components/Auth";
-import Header from "./components/shoppingCart/Header";
-import Home from "./components/shoppingCart/Home";
+import Header from "./components/shopping-context-reducer/Header";
+//import Home from "./components/shoppingCart/Home";
 
 function App() {
   // const authContext = useContext(AuthContext);
@@ -18,14 +18,14 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div>
+      {/*  <div>
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
         <Routes>
           <Route path="/cart" element={<CartPage />} />
         </Routes>
-      </div>
+      </div> */}
     </BrowserRouter>
   );
 }
