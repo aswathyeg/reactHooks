4a308db73586a13b0417bcec1d55d88fcf6ea19e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-11 14:48:06 +0530

    added search

--- src/components/shopping-context-reducer/Header.js
+++ src/components/shopping-context-reducer/Header.js
@@ -1,12 +1,20 @@
-import { Container } from "react-bootstrap";
+import { Container, FormControl } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 const Header = () => {
   return (
-    <Navbar bg="dark">
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
+        {/* for logo */}
         <Navbar.Brand>
           <a href="/">Shopping cart</a>
         </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            className="m-auto"
+            placeholder="Search a product"
+            style={{ width: 500 }}
+          ></FormControl>
+        </Navbar.Text>
       </Container>
     </Navbar>
   );
