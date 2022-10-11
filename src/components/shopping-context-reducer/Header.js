13c6df4cd9f49bd1ba14409a3f1bd818684b9d5a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-11 13:53:00 +0530

    added Navbar

---
+++ src/components/shopping-context-reducer/Header.js
@@ -0,0 +1,10 @@
+import { Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+const Header = () => {
+  return (
+    <Navbar bg="dark">
+      <Container>Shopping cart</Container>
+    </Navbar>
+  );
+};
+export default Header;
