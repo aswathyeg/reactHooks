3e3c2a3f32483a5c27d12bc7ce85394d61bdc825
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-14 14:31:44 +0530

    added context

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import AuthContextProvider from "./context/auth-context";
-import Context from "./components/shoppingCart/Context";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Context from "./components/shopping-context-reducer/context/Context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Context>
