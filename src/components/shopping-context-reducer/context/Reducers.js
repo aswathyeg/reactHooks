012e008194a37b37c792c907e6e2908b2191701d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-14 14:35:02 +0530

    updated

--- src/components/shopping-context-reducer/context/Reducers.js
+++ src/components/shopping-context-reducer/context/Reducers.js
@@ -1,9 +1,9 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    case "AddtoCart": {
-    }
-    case "Remove from Cart": {
-    }
+    // case "AddtoCart": {
+    // }
+    // case "Remove from Cart": {
+    // }
     default:
       return state;
   }
