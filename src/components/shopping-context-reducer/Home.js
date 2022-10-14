85e4c9e0659ff2e52caf5f972b309205f1f849ea
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-14 14:33:32 +0530

    added map function

--- src/components/shopping-context-reducer/Home.js
+++ src/components/shopping-context-reducer/Home.js
@@ -1,4 +1,21 @@
+import { CartState } from "./context/Context";
+import "./Styles.css";
+
 const Home = () => {
-  return <div></div>;
+  const {
+    state: { products },
+  } = CartState();
+  console.log(products);
+
+  return (
+    <div className="home">
+      <div className="productContainer">
+        {products.map((prod) => (
+          <span>{prod.name}</span>
+        ))}
+      </div>
+      <p>Home</p>
+    </div>
+  );
 };
 export default Home;
