25729cb32e397b747023246ad07ffd5e28dbce11
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-17 17:19:45 +0530

    added card

--- src/components/shopping-context-reducer/SingleProduct.js
+++ src/components/shopping-context-reducer/SingleProduct.js
@@ -1,4 +1,26 @@
+import { Button, Card } from "react-bootstrap";
+import Rating from "./Rating";
 const SingleProduct = ({ prod }) => {
-  return <div>{prod.name}</div>;
+  return (
+    <div className="products">
+      <Card>
+        <Card.Img variant="top" src={prod.image} alt={prod.name} />
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>${prod.price.split(".")[0]}</span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div> 4 days Delivery</div>
+            )}
+            <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          <Button variant="danger">Remove from Cart</Button>
+          <Button>Add to Cart</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 export default SingleProduct;
