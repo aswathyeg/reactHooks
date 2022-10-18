a4bed24c9526c121fc7a192dfa1979754de34cd0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-18 14:12:13 +0530

    added reducers

--- src/components/shopping-context-reducer/context/Reducers.js
+++ src/components/shopping-context-reducer/context/Reducers.js
@@ -1,9 +1,12 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    // case "AddtoCart": {
-    // }
-    // case "Remove from Cart": {
-    // }
+    case "AddtoCart":
+      return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
+    case "Remove from Cart":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
     default:
       return state;
   }
