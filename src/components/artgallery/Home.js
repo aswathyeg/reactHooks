8a077fc39909858254d744ad69ceebf3ac7c763f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-11-30 12:42:46 +0530

    added product array

--- src/components/artgallery/Home.js
+++ src/components/artgallery/Home.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
 import "./Styles.css";
+import SingleProduct from "./SingleProduct";
 faker.seed(100);
 const Home = () => {
-  const products = [...Array(20)].map(() => ({
+  const productsArray = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
     //image: faker.random.image.fashion(),
     image: faker.image.image(),
   }));
-  return <div>Home</div>;
+  const [products] = useState(productsArray);
+  console.log(productsArray);
+  return (
+    <div>
+      {products.map((prod) => (
+        <SingleProduct prod={prod} key={prod.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
