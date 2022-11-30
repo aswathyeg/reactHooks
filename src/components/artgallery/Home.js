a4277e8d064b49693b4c0154770a24fa591525c1
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-11-30 12:08:07 +0530

    added faker

--- src/components/artgallery/Home.js
+++ src/components/artgallery/Home.js
@@ -1,6 +1,15 @@
 import React from "react";
-
+import { faker } from "@faker-js/faker";
+import "./Styles.css";
+faker.seed(100);
 const Home = () => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    //image: faker.random.image.fashion(),
+    image: faker.image.image(),
+  }));
   return <div>Home</div>;
 };
 
