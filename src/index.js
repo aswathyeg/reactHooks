0c1a73138a90ae1d10de5bedc53e6bc564122cb2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-11-30 12:07:48 +0530

    changed component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 // import AuthContextProvider from "./context/auth-context";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Context from "./components/shopping-context-reducer/context/Context";
+import Context from "./components/artgallery/context/Context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Context>
