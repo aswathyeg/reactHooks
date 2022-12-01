e0ac6dbaa0eba0c49e66a5f79dcea98291a78394
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-01 17:09:45 +0530

    added cases

---
+++ src/components/artgallery/context/Reducers.js
@@ -0,0 +1,13 @@
+export const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "Add to cart":
+      return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
+    case "Remove from cart":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+    case "change quantity in the cart":
+      return {};
+  }
+};
