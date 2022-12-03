fdc8cd0ab78513531331d5a2b265a9f9afc3594a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-03 16:37:16 +0530

    added style

--- src/TodoListApp/components/Home.js
+++ src/TodoListApp/components/Home.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { Container, FormControl, Navbar } from "react-bootstrap";
-
+import "../Styles.css";
 const Home = () => {
   const [state, setState] = useState();
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState([]);
   const handleClick = () => {
     setValue(state);
   };
   return (
-    <Container>
+    <Container className="home">
       <Navbar.Text>
         <FormControl
           onChange={(e) => {
@@ -18,7 +18,11 @@ const Home = () => {
         <br></br>
         <button onClick={handleClick}>Add</button>
         <br></br>
-        <span>{value}</span>
+        <span>
+          {value.map((c) => {
+            return <div>{c}</div>;
+          })}
+        </span>
       </Navbar.Text>
     </Container>
   );
