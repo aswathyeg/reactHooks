5633dbb8b2b9f41467d915e16ca9404762091399
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-08 22:22:57 +0530

    changed components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-//import CartPage from "./components/shoppingCart/CartPage";
-//import Cart from "./components/shoppingCart/CartPage";
+// import CartPage from "./components/shoppingCart/CartPage";
+// import Cart from "./components/shoppingCart/CartPage";
 
-// import Header from "./components/shopping-context-reducer/Header";
-// import Home from "./components/shopping-context-reducer/Home";
-// import Cart from "./components/shopping-context-reducer/Cart";
-import Header from "./TodoListApp/components/Header";
-import Home from "./TodoListApp/components/Home";
+import Header from "./components/shopping-context-reducer/Header";
+import Home from "./components/shopping-context-reducer/Home";
+import Cart from "./components/shopping-context-reducer/Cart";
+// import Header from "./TodoListApp/components/Header";
+// import Home from "./TodoListApp/components/Home";
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Header />
-
-    //   <div>
-    //     <Routes>
-    //       <Route exact path="/" element={<Home />} />
-    //     </Routes>
-
-    //     <Routes>
-    //       <Route path="/cart" element={<Cart />} />
-    //     </Routes>
-    //   </div>
-    // </BrowserRouter>
-
-    <div>
+    <BrowserRouter>
       <Header />
-      <Home />
-    </div>
+
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+
+        <Routes>
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
